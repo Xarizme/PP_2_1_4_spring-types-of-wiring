b62a65c3606287c93758139ecbc8a758bfdc24da
Xarizme <Dodger19@yandex.ru>
2023-12-19 13:58:40 +0300

    Решение задачи

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,12 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private final Duck5 duck;
+    private Duck5 duck;
     @Autowired
-    public Rabbit4(Duck5 duck) {
+    public void setDuck(Duck5 duck) {
         this.duck = duck;
     }
-
 
     @Override
     public String toString() {
